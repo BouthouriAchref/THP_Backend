13454c14e99ab9621ee05e39942cb6c02a1da943
Achref Bouthouri <Achref.Bouthouri11@gmail.com>
2021-05-27 16:04:05 +0100

    changeServerDB

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     jwtSecret: 'TunisiaHiddenPlaces',
-    db: 'mongodb://localhost/TunisiaHiddenPlaces',
-    //db: 'mongodb+srv://AchrefBouthouri:AZERTY123@cluster0.6n6dc.mongodb.net/TunisianHiddenPlaces?retryWrites=true&w=majority',
+    //db: 'mongodb://localhost/TunisiaHiddenPlaces',
+    db: 'mongodb+srv://AchrefBouthouri:AZERTY123@cluster0.6n6dc.mongodb.net/TunisianHiddenPlaces?retryWrites=true&w=majority',
     cloudinary_name: 'tunisia-hidden-places',
     cloudinary_api_key: '291712196456257',
     cloudinary_secret_key: '_ulif48EaiUlKFXwLtNAFvCQZi8'
