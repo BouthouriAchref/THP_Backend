8c587906556d70e8a160465d22228ae326e231a5
Achref Bouthouri <Achref.Bouthouri11@gmail.com>
2021-06-07 20:33:50 +0100

    modify CategoryUpload

--- src/routes/Category.js
+++ src/routes/Category.js
@@ -4,7 +4,7 @@ const category = require('../controller/category-controller')
 const uploads = require('../middleware/multer')
 
 CategoryRoute.post('/addCat', category.addCategory);
-CategoryRoute.put('/:catId', uploads.upload.single('image'), category.uploadImageCategory);
+CategoryRoute.post('/:catId', uploads.upload.single('image'), category.uploadImageCategory);
 CategoryRoute.get('/', category.getAllCategory);
 CategoryRoute.get('/:catId', category.getCategoryById);
 CategoryRoute.delete('/deleteCat/:catId', category.deleteCategory);
