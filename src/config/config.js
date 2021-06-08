ccc2dfed29f5f61a76b03cded3d11a9b69741488
Achref Bouthouri <Achref.Bouthouri11@gmail.com>
2021-06-08 09:22:51 +0100

    modify DBServer

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     jwtSecret: 'TunisiaHiddenPlaces',
-    db: 'mongodb://localhost/TunisiaHiddenPlaces',
-    //db: 'mongodb+srv://AchrefBouthouri:AZERTY123@cluster0.6n6dc.mongodb.net/TunisianHiddenPlaces?retryWrites=true&w=majority',
+    //db: 'mongodb://localhost/TunisiaHiddenPlaces',
+    db: 'mongodb+srv://AchrefBouthouri:AZERTY123@cluster0.6n6dc.mongodb.net/TunisianHiddenPlaces?retryWrites=true&w=majority',
     path: "https://tunisian-hidden-places.herokuapp.com/",
     //path: "http://localhost:3000/",
     cloudinary_name: 'tunisia-hidden-places',
